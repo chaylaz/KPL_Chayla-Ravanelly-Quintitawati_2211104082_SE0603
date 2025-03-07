fa4fc06643368d3ce30cf45cf009d28aa7de3c7a
chaylaz <chayla0504@gmail.com>
2025-03-07 08:09:37 +0700

    Menambahkan Praktikum Modul 2.

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,15 @@
+var name = "Chayla Ravanelly Quintitawati"
+const nim = 2211104082
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "Nelly"
+
+let fullName = "Chayla Ravanelly Quintitawati"
