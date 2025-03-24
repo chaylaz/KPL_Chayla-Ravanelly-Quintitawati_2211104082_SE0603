18d0493a16038064030ecd4cee048f70ca349d43
chaylaz <chayla0504@gmail.com>
2025-03-24 14:50:47 +0700

    Menambahkan class SimpleDataBase.

--- 05_Generics/TP & Jurnal/Jurnal_Modul5_2211104082/Penjumlahan.js
+++ 05_Generics/TP & Jurnal/Jurnal_Modul5_2211104082/Penjumlahan.js
@@ -5,10 +5,34 @@ class Penjumlahan {
     }
 }
 
-// NIM : 2211104082 -> Menggunakan float
+class SimpleDataBase {
+    constructor() {
+        this.storedData = [];
+        this.inputDates = [];
+    }
+
+    AddNewData(data) {
+        this.storedData.push(data);
+        this.inputDates.push(new Date().toUTCString());
+    }
+
+    PrintAllData() {
+        this.storedData.forEach((data, index) => {
+            console.log(`Data ${index + 1} berisi : ${data}, yang disimpan pada waktu UTC : ${this.inputDates[index]}`);
+        });
+    }
+}
+
+// NIM : 2211104082 -> Menggunakan float 
 let angka1 = 22.0;
 let angka2 = 11.0;
 let angka3 = 10.0;
 
 let hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
 console.log(`Hasil penjumlahan : ${hasil}`);
+
+let database = new SimpleDataBase();
+database.AddNewData(angka1);
+database.AddNewData(angka2);
+database.AddNewData(angka3);
+database.PrintAllData();
