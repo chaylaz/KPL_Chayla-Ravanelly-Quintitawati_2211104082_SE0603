fc36ccb8caee53029a90792baaae6646bd721ff4
chaylaz <chayla0504@gmail.com>
2025-03-24 14:31:04 +0700

    Menambahkan class DataGeneric.

--- 05_Generics/TP & Jurnal/TP_Modul5_2211104082/HaloGeneric.js
+++ 05_Generics/TP & Jurnal/TP_Modul5_2211104082/HaloGeneric.js
@@ -4,5 +4,18 @@ class HaloGeneric {
     }
 }
 
+class DataGeneric {
+    constructor(data) {
+        this.data = data;
+    }
+
+    PrintData() {
+        console.log(`Data yang tersimpan adalah : ${this.data}`);
+    }
+}
+
 const halo = new HaloGeneric();
-halo.SapaUser("Chayla"); // Nama praktikan
+halo.SapaUser("Chayla"); // nama panggilan praktikan
+
+const data = new DataGeneric("2211104082"); // NIM praktikan
+data.PrintData();
